63081216879e6b720c4f2bdfc721f1a7d869a8bb
so dam <129500149+1123ksd1@users.noreply.github.com>
2025-02-20 10:14:12 +0900

    Add files via upload

---
+++ source/repos/main.c
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS 
+#include<stdbool.h>//잘 사용하지 않는 bool의 형태를 include형으로 정의를 한것,간단하다는장점
+
+//&:특정 비트를 0으로 만들때
+//|:특정 비트를 1로 만들 때
+// ^:특정 비트의 상태를 반전
+     
+
+
+
+    int main() {
+        int i, j;
+        for(i=0;i<5;i++){
+            for (j = 0; j < 5; j++) {
+                if ((i = j) || (i + j == 4))
+                    printf("*");
+                else
+                    printf("");
+            }
+           
+        }
+    }
